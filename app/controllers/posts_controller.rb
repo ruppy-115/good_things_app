673b8e4494fa3f853222e60e05f6cbf350f2de58
坪子 月渚 <tsuboko@72-92446.local>
2026-01-30 17:10:34 +0900

    add:投稿作成処理を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,20 @@ class PostsController < ApplicationController
     def new
         @post = Post.new
     end
+
+    def create
+        @post = current_user.posts.build(post_params)
+        if @post.save
+            redirect_to posts_path, success: "今日の記録を作成しました"
+        else
+            flash.now[:danger] = "記録を作成できませんでした"
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:body)
+    end
 end
