c692b4b5a148fb0c934bc67c26cacfdb9894d680
坪子 月渚 <tsuboko@72-92446.local>
2026-02-16 00:18:39 +0900

    エラー修正

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,11 +1,9 @@
 // app/javascript/controllers/index.js
 
-import { application } from "controllers/application"
+import { application } from "./application"
 
-// "controllers/ファイル名" という形式でインポート
-import NotebookFlipController from "controllers/notebook_flip_controller"
-import HelloController from "controllers/hello_controller"
+import NotebookFlipController from "./notebook_flip_controller"
+import HelloController from "./hello_controller"
 
-// 手動で登録（HTMLの data-controller="notebook-flip" と紐付ける）
 application.register("notebook-flip", NotebookFlipController)
 application.register("hello", HelloController)
