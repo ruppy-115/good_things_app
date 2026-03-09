958c144eac17438c19919e4e332afef594011ef8
坪子 月渚 <tsuboko@72-92446.local>
2026-03-09 22:23:47 +0900

    feat:自分の記録ページのデザイン変更

--- app/controllers/my_posts_controller.rb
+++ app/controllers/my_posts_controller.rb
@@ -1,5 +1,13 @@
 class MyPostsController < ApplicationController
-    def index
-        @posts = current_user.posts.includes(:user).order(created_at: :desc)
+  def index
+    @current_date = params[:date] ? Date.parse(params[:date]) : Date.current
+
+    if @current_date.month <= 6
+      @start_date = @current_date.beginning_of_year
+    else
+      @start_date = @current_date.beginning_of_year + 6.months
     end
+
+    @posts = current_user.posts.where(created_at: @current_date.all_month).order(created_at: :desc)
+  end
 end
