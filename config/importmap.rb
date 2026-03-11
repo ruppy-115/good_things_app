3dbce84dd818ec2ce7082aa1987298bd30ae33c3
坪子 月渚 <tsuboko@72-92446.local>
2026-03-11 16:34:32 +0900

    dockerfileの設定変更

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.23
-pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.23
-pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.1.200
+pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js"
+pin "@hotwired/turbo", to: "@hotwired--turbo.js"
+pin "@rails/actioncable/src", to: "@rails--actioncable--src.js"
 
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
