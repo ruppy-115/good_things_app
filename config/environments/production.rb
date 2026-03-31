1e0ee22e8bb1680855cace143e60cd6d457520ce
坪子 月渚 <tsuboko@72-92446.local>
2026-03-31 22:42:52 +0900

    本番環境について追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,4 +99,18 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: "good-things-app.onrender.com" }
+
+  config.action_mailer.smtp_settings = {
+    address:        "smtp.resend.com",
+    port:           587,
+    user_name:      "resend",
+    password:       ENV["RESEND_API_KEY"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
